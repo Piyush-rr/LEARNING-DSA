56ae51b02f64c2abfdf334326d80ee61141630e3
Piyush-rr <piyushkumar201970@gmail.com>
2024-03-19 22:05:22 +0530

    pointers in cpp

---
+++ POINTERS/productOfTwoNumbers.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int *p,*q,a,b,sum;
+    cout<<"enter the value of a and b"<<endl;
+    cin>>a>>b;
+    p=&a;
+    q=&b;
+    sum=*p**q;
+    cout<<"sum of two numbers is :"<<sum;
+    return 0;
+}
+
+
+// using function
+// #include<iostream>
+// using namespace std;
+// void multiply(int *p,int *q,int *result){
+// *result=(*p)*(*q);
+// }
+// int main(){
+//     int p,q,result;
+//     cout<<"enter the first and seconde numbers"<<endl;
+//     cin>>p>>q;
+//    multiply(&p,&q,&result);
+//    cout<<result;
+//     return 0;
+// }
---
+++ POINTERS/sumOfTwoNum.cpp
@@ -0,0 +1,26 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int *p,*q,result,a,b;
+//     cout<<"enter the two numbers "<<endl;
+//     cin>>a>>b;
+//     p=&a;
+//     q=&b;
+//     result=*p+*q;
+//     cout<<"sum of the numbers is :"<<result;
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+void sumOfNumbers(int *num1,int *num2,int *result){
+    *result=*num1+*num2;
+}
+int main(){
+    int num1,num2,result;
+    cout<<"enter the first and second numbers "<<endl;
+    cin>>num1>>num2;
+    sumOfNumbers(&num1,&num2,&result);
+        cout<<"sum of numbers is :"<<result;
+    return 0;
+}
